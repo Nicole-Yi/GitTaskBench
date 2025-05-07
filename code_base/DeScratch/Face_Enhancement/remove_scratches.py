f0c2c3b3748c18a8a46cb14602007c1351a420b3
i-youwang <i-youwang@stepfun.com>
2025-05-08 00:35:01 +0800

    SWE-agent task DeScratch_01.md: Post-run commit

---
+++ code_base/DeScratch/Face_Enhancement/remove_scratches.py
@@ -0,0 +1,5 @@
+
+import os
+
+if __name__ == "__main__":
+    os.system("python /data/data/agent_test_codebase/GitTaskBench/code_base/DeScratch/run.py --input_folder /data/data/agent_test_codebase/GitTaskBench/queries/DeScratch_01/input --output_folder /data/data/agent_test_codebase/GitTaskBench/eval_automation/output/DeScratch_01 --with_scratch")
